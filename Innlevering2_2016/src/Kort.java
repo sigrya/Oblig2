6382363f3fd23b0312604c27a5c3b79a5152c039
Sigrid Ryan <sigrya@gmail.com>
2016-03-02 13:03:25 +0100

    versjon 1 ferdig

--- Innlevering2_2016/src/Kort.java
+++ Innlevering2_2016/src/Kort.java
@@ -1,58 +1,81 @@
 public abstract class Kort {
-	//Set Default values for variables
+	// Set Default values for variables
 	private String fornavn;
 	private String etternavn;
 	private int pinkode;
 	private int kortnummer;
 	private boolean sperretKort = false;
 	private static int antallKort;
-	
-	//Return fornavn
+
+	public Kort(String navn, int pin) {
+		String[] fulnavn = navn.split(" ");
+		fornavn = fulnavn[0];
+		etternavn = fulnavn[1];
+		pinkode = pin;
+		kortnummer = ++antallKort;
+	}
+
+	// Return fornavn
 	public String getFornavn() {
 		return fornavn;
 	}
-	//Set fornavn
+
+	// Set fornavn
 	public void setFornavn(String fornavn) {
 		this.fornavn = fornavn;
 	}
-	//Return etternavn
+
+	// Return etternavn
 	public String getEtternavn() {
 		return etternavn;
 	}
-	//Set etternavn
+
+	// Set etternavn
 	public void setEtternavn(String etternavn) {
 		this.etternavn = etternavn;
 	}
-	//Set Pin
+
+	// Set Pin
 	public int getPinkode() {
 		return pinkode;
 	}
-	//Return pin
+
+	// Return pin
 	public void setPinkode(int pinkode) {
 		this.pinkode = pinkode;
 	}
-	//Set kortnummer
+
+	// Set kortnummer
 	public int getKortnummer() {
 		return kortnummer;
 	}
-	//Return kortnummer
+
+	// Return kortnummer
 	public void setKortnummer(int kortnummer) {
 		this.kortnummer = kortnummer;
-	}	
-	//return sperretKort
+	}
+
+	// return sperretKort
 	public boolean isSperretKort() {
 		return sperretKort;
 	}
-	//Set new filled
+
+	// Set new filled
 	public void setSperretKort(boolean sperretKort) {
 		this.sperretKort = sperretKort;
 	}
-	//Abstract method
-		public abstract void sjekkPin();
-		
-	//StingtoSting
+
+	// Abstract method
+	public abstract boolean sjekkPin(int pin);
+
+	// StingtoSting
 	public String toString() {
-		return "Fornavn: " + fornavn + " \nEtternavn: " + etternavn + " \nKortnummer: " + kortnummer + "\nSperret: " +sperretKort;
+		return "Fornavn: " + fornavn + " \nEtternavn: " + etternavn
+				+ " \nKortnummer: " + kortnummer + "\nSperret: " + sperretKort;
 	}
-	
+
+	public String getNavn() {
+		return getFornavn() + " " + getEtternavn();
+	}
+
 }
